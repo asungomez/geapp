9ae45413a8bac786fcdf5263261685b0a95d93de
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2022-09-28 12:12:08 +0200

    Add Amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { theme } from '../styles/theme';
 import { ThemeProvider } from '@mui/material';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 };
 
