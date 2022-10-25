1097ddf70cff809a65cffc7ce4dc237efb430f30
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2022-10-25 13:01:14 +0200

    Separate videos folder between e2e and component tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,10 +6,12 @@ export default defineConfig({
       framework: 'next',
       bundler: 'webpack',
     },
+    videosFolder: 'cypress/videos/component',
   },
 
   e2e: {
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
+    videosFolder: 'cypress/videos/e2e',
   },
 });
