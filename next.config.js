baf31ab9dc856ea2428569b0121b4a5cf6668f66
Maria.GomezCastro <Maria.GomezCastro@eu.jll.com>
2022-11-02 10:58:49 +0100

    Restore next-i18next config file and copy it in the amplify config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: { defaultLocale: 'es', locales: ['en', 'es'] },
+  i18n,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
